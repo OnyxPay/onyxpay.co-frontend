b4ee3a2fb696b78683ae9594ceb430c7dba9047f
Vladimir Stavitskiy <vladimir.stavitskiy@apriorit.com>
2019-06-04 19:42:45 +0300

    add country-list

--- src/pages/deposit/index.js
+++ src/pages/deposit/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Formik } from "formik";
-import { Card, Button, Input, Row, Col, Form, Select } from "antd";
+import { Card, Button, Input, Form, Select } from "antd";
 import { PageTitle } from "../../components";
-import { country_list } from "../../assets/country_list";
+// import { country_list } from "../../assets/country_list";
 import Actions from "../../redux/actions";
+import { getData as getCountriesData } from "country-list";
 
 const { Option } = Select;
 class Deposit extends Component {
@@ -18,18 +19,25 @@ class Deposit extends Component {
 		resetForm();
 	};
 
-	handleSelectChange = setFieldValue => (value, option) => {
-		setFieldValue("country_id", value);
+	handleAssetChange = setFieldValue => (value, option) => {
+		console.log("handleAssetChange", value);
+		setFieldValue("asset", value);
+	};
+
+	handleCountryChange = setFieldValue => (value, option) => {
+		console.log("handleCountryChange", value);
+		setFieldValue("country", value);
 	};
 
 	render() {
+		const { assets } = this.props;
 		return (
 			<>
 				<PageTitle>Deposit</PageTitle>
 				<Card>
 					<Formik
 						onSubmit={this.handleFormSubmit}
-						initialValues={{ asset: "", amount: "", country: "" }}
+						initialValues={{ asset: "oUSD", amount: "", country: "" }}
 						// validate={values => {
 						// 	let errors = {};
 						// 	if (!values.agentId) {
@@ -50,52 +58,76 @@ class Deposit extends Component {
 						}) => {
 							return (
 								<form onSubmit={handleSubmit}>
-									<Row gutter={16}>
-										<Col md={24} lg={12}>
-											<Form.Item
-												label="Country"
-												required
-												validateStatus={errors.country_id && touched.country_id ? "error" : ""}
-												help={errors.country_id && touched.country_id ? errors.country_id : ""}
-											>
-												<Select
-													size="large"
-													showSearch
-													name="country_id"
-													placeholder="Select a country"
-													optionFilterProp="children"
-													value={values.country_id}
-													onChange={this.handleSelectChange(setFieldValue)}
-													filterOption={(input, option) =>
-														option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-													}
-													disabled={isSubmitting}
-												>
-													{country_list.map((country, index) => {
-														return (
-															<Option key={index} value={index}>
-																{country}
-															</Option>
-														);
-													})}
-												</Select>
-											</Form.Item>
-										</Col>
-										<Col md={24} lg={12}>
-											<Form.Item label="Amount" required>
-												<Input
-													size="large"
-													name="amount"
-													type="number"
-													placeholder="enter amount"
-													value={values.amount}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													disabled={isSubmitting}
-												/>
-											</Form.Item>
-										</Col>
-									</Row>
+									<Form.Item
+										label="Asset"
+										required
+										validateStatus={errors.asset && touched.asset ? "error" : ""}
+										help={errors.asset && touched.asset ? errors.asset : ""}
+									>
+										<Select
+											size="large"
+											showSearch
+											name="asset"
+											placeholder="Select an asset"
+											optionFilterProp="children"
+											value={values.asset}
+											onChange={this.handleAssetChange(setFieldValue)}
+											filterOption={(input, option) =>
+												option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+											}
+											disabled={isSubmitting}
+										>
+											{assets.map((asset, index) => {
+												return (
+													<Option key={index} value={asset}>
+														{asset}
+													</Option>
+												);
+											})}
+										</Select>
+									</Form.Item>
+
+									<Form.Item
+										label="Country"
+										required
+										validateStatus={errors.country && touched.country ? "error" : ""}
+										help={errors.country && touched.country ? errors.country : ""}
+									>
+										<Select
+											size="large"
+											showSearch
+											name="country"
+											placeholder="Select a country"
+											optionFilterProp="children"
+											value={values.country}
+											onChange={this.handleCountryChange(setFieldValue)}
+											filterOption={(input, option) =>
+												option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+											}
+											disabled={isSubmitting}
+										>
+											{getCountriesData().map((country, index) => {
+												return (
+													<Option key={country.code} value={country.code}>
+														{country.name}
+													</Option>
+												);
+											})}
+										</Select>
+									</Form.Item>
+
+									<Form.Item label="Amount" required>
+										<Input
+											size="large"
+											name="amount"
+											type="number"
+											placeholder="Enter an amount"
+											value={values.amount}
+											onChange={handleChange}
+											onBlur={handleBlur}
+											disabled={isSubmitting}
+										/>
+									</Form.Item>
 
 									<Button type="primary" htmlType="submit" disabled={isSubmitting}>
 										Submit
@@ -114,6 +146,7 @@ export default connect(
 	state => {
 		return {
 			user: state.user,
+			assets: state.assets.list,
 		};
 	},
 	{ getAssetsList: Actions.assets.getAssetsList }
