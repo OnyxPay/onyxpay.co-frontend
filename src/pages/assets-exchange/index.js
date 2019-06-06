56d5f832473c551bca1270df0b025c279bc1c09b
Anna Korunska <akorunska@gmail.com>
2019-06-06 11:44:10 +0300

    displaying standart title in 'Exchange Assets' tab

--- src/pages/assets-exchange/index.js
+++ src/pages/assets-exchange/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { PageTitle } from "../../components";
+import Actions from "../../redux/actions";
 // import { Tabs } from "antd";
 
 // const { TabPane } = Tabs;
 
-const AssetsExchange = () => {
-	return <div>It's time to exchange your assets!</div>;
-};
+class AssetsExchange extends Component {
+	componentDidMount() {
+		const { getAssetsList } = this.props;
+		getAssetsList();
+	}
+	render() {
+		const { assets } = this.props;
+		return <PageTitle>AssetsExchange</PageTitle>;
+	}
+}
 
-export default AssetsExchange;
+export default connect(
+	state => {
+		return {
+			user: state.user,
+			assets: state.assets.list,
+		};
+	},
+	{ getAssetsList: Actions.assets.getAssetsList }
+)(AssetsExchange);
