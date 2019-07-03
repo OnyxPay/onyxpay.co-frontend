9153515cf2cb114f49974aab619803e33918ea93
oleh.hinko <44670145+OlehHinko@users.noreply.github.com>
2019-07-03 17:21:23 +0300

    updaye index.js

--- src/pages/admin-panel/assets/index.js
+++ src/pages/admin-panel/assets/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { PageTitle } from "../../../components";
-import AssetsList from "./assetsList";
 import AssetsList from "./AssetsList";
 
 export default class Assets extends Component {
