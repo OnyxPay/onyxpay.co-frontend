6f2cfc7cc2fd9446a9f34baae413db6b69185341
Anna Korunska <akorunska@gmail.com>
2019-07-05 16:20:22 +0300

    refactored exchange transaction to use gas compensator

--- src/api/exchange.js
+++ src/api/exchange.js
@@ -1,19 +1,10 @@
 import { ParameterType, utils } from "ontology-ts-sdk";
 import { unlockWalletAccount } from "./wallet";
-import { getStore } from "../store";
-import { resolveContractAddress } from "../redux/contracts";
-import { createTrx, signTrx, sendTrx } from "./bc";
-import { timeout } from "promise-timeout";
-import { notifyTimeout } from "./constants";
+import { addSignAndSendTrx, createAndSignTrxViaGasCompensator } from "./bc";
 import { getWallet, getAccount } from "../api/wallet";
 
 export async function exchangeAssets(values) {
-	const store = getStore();
-	const address = await store.dispatch(resolveContractAddress("Exchange"));
-	if (!address) {
-		throw new Error("Unable to get address of Exchange smart-contract");
-	}
-	const { pk, accountAddress } = await unlockWalletAccount();
+	const { pk } = await unlockWalletAccount();
 	const walletDecoded = getWallet(values.wallet);
 	const account = getAccount(walletDecoded);
 
@@ -41,14 +32,12 @@ export async function exchangeAssets(values) {
 		},
 	];
 	console.log(params);
-	const trx = createTrx({
-		funcName: "ExchangeAssets",
-		params: params,
-		contractAddress: address,
-		accountAddress,
-	});
 
-	signTrx(trx, pk);
-	const res = await timeout(sendTrx(trx, false, true), notifyTimeout);
-	return res;
+	const serializedTrx = await createAndSignTrxViaGasCompensator(
+		"Exchange",
+		"ExchangeAssets",
+		params
+	);
+
+	return await addSignAndSendTrx(serializedTrx, pk);
 }
