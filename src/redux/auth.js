9a25ce885c79e62c9e6988f45f0b75ee9c1e88f1
ak-toktor <ak-toktor@gmail.com>
2019-07-16 20:41:10 +0300

    refactoring after review

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -22,6 +22,11 @@ export const authReducer = (state = initialState, action) => {
 			localStorage.setItem("OnyxAddr", action.payload.OnyxAddr);
 			localStorage.setItem("logged_in", true);
 			return action.payload;
+		case LOG_OUT:
+			localStorage.removeItem("OnyxAuth");
+			localStorage.removeItem("OnyxAddr");
+			localStorage.removeItem("logged_in");
+			return null;
 		default:
 			return state;
 	}
@@ -80,9 +85,4 @@ export const confirmEmail = values => async (dispatch, getState) => {
 
 export const logOut = notReload => (dispatch, getState) => {
 	dispatch({ type: LOG_OUT });
-
-	if (!notReload) {
-		dispatch(push("/login"));
-		window.location.reload();
-	}
 };
