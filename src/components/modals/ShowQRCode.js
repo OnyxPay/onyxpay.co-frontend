94174fb64a379072c2114ea35a137171a3640b03
Oleh Hinko <oleh.hinko@apriorit.com>
2019-07-18 13:51:10 +0300

    refactor to the functional component

--- src/components/modals/ShowQRCode.js
+++ src/components/modals/ShowQRCode.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import QRCode from "qrcode.react";
 import { Modal } from "antd";
 
-class ShowQRCode extends Component {
-	render() {
-		const { isModalVisible, hideModal, link } = this.props;
-		return (
-			<>
-				<Modal
-					visible={isModalVisible}
-					onCancel={hideModal}
-					onOk={hideModal}
-					className="modal-qrcode"
-				>
-					<QRCode size={256} value={link} />
-				</Modal>
-			</>
-		);
-	}
+export default function ShowQRCode(props) {
+	const { isModalVisible, hideModal, link } = props;
+	return (
+		<>
+			<Modal
+				visible={isModalVisible}
+				onCancel={hideModal}
+				s
+				onOk={hideModal}
+				className="modal-qrcode"
+			>
+				<QRCode size={256} value={link} />
+			</Modal>
+		</>
+	);
 }
-
-export default ShowQRCode;
--- src/pages/profile/ReferralLink.js
+++ src/pages/profile/ReferralLink.js
@@ -1,60 +1,51 @@
-import React, { Component } from "react";
-import { Input, Icon, message, Button } from "antd";
+import React, { useState } from "react";
+import { Input, Icon, Button } from "antd";
 import { BackendUrl } from "api/constants";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import ShowQRCode from "components/modals/ShowQRCode";
+import { showNotification } from "components/notification";
 
-const modals = {
-	ADD_SHOW_QR_CODE: "ADD_SHOW_QR_CODE",
-};
-
-export default class ReferralLink extends Component {
-	state = {
-		ADD_SHOW_QR_CODE: false,
+export default function ReferralLink() {
+	const [SHOW_QR_CODE_MODAL, setToggleQRCode] = useState(false);
+	const showModalQRCode = () => {
+		setToggleQRCode(true);
 	};
-
-	showModalQRCode = type => () => {
-		this.setState({ ADD_SHOW_QR_CODE: true });
+	const hideModalQRCode = () => {
+		setToggleQRCode(false);
 	};
+	const referralCode = localStorage.getItem("OnyxAddr");
+	const link = BackendUrl + "?rcode=" + referralCode;
 
-	hideModalQRCode = type => () => {
-		this.setState({ ADD_SHOW_QR_CODE: false });
-	};
-
-	render() {
-		const { ADD_SHOW_QR_CODE } = this.state;
-		const referralCode = localStorage.getItem("OnyxAddr");
-		const link = BackendUrl + "?rcode=" + referralCode;
-		return (
-			<div>
-				<h3>
-					<b>Referral info</b>
-				</h3>
-				<div className="referral-info">
-					<Input
-						addonBefore="Referral link:"
-						value={link}
-						style={{ border: "none" }}
-						className="referral-link-input"
-						suffix={
-							<CopyToClipboard
-								text={link}
-								onCopy={() => message.info("Referral link already in the clipboard.", 5)}
-							>
-								<Icon type="copy" style={{ marginLeft: 5, width: 16, height: 16 }} />
-							</CopyToClipboard>
-						}
-					/>
-					<Button onClick={this.showModalQRCode()} className="btn-show-qrcode" type="primary">
-						QR Code
-					</Button>
-				</div>
-				<ShowQRCode
-					link={link}
-					isModalVisible={ADD_SHOW_QR_CODE}
-					hideModal={this.hideModalQRCode(modals.ADD_SHOW_QR_CODE)}
+	return (
+		<div>
+			<h3>
+				<b>Referral info</b>
+			</h3>
+			<div className="referral-info">
+				<Input
+					addonBefore="Referral link:"
+					value={link}
+					style={{ border: "none" }}
+					className="referral-link-input"
+					suffix={
+						<CopyToClipboard
+							text={link}
+							onCopy={() =>
+								showNotification({
+									type: "info",
+									msg: "Referral link has been copied in the clipboard.",
+								})
+							}
+						>
+							<Icon type="copy" style={{ marginLeft: 5, width: 16, height: 16 }} />
+						</CopyToClipboard>
+					}
 				/>
+				<Button onClick={showModalQRCode} className="btn-show-qrcode" type="primary">
+					QR Code
+				</Button>
 			</div>
-		);
-	}
+			<ShowQRCode link={link} isModalVisible={SHOW_QR_CODE_MODAL} hideModal={hideModalQRCode} />
+		</div>
+	);
 }
