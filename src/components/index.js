0fe9a6044a8d98c216f61addfcbaf8daf6af2681
Anna Korunska <36419787+akorunska@users.noreply.github.com>
2019-08-02 11:15:52 +0300

    [Onxp 837] send ui and transactions list (#137)
    * table header for exchange transaction history
    
    * added request to api and logic of table loading and pagination
    
    * finished work on displaying exchange history in a table
    
    * fetching transactions every 30 seconds
    
    * transactions table component
    
    * refactored transactions list to be used in both exchange history and operations widget
    
    * refactored transactions list to be used in both exchange history and operations widget
    
    * exchange history and operation history now use one component for displaying tx lists
    
    * edited status displaying for transaction lists
    
    * removed displaying status from operations log
    
    * changed format of displaying dates
    
    * removed console logs
    
    * refactoring after review
    
    * restored page size of 10

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,16 @@
 import { PageTitle, Container } from "./styled";
-import TransactionsTable from "./TransactionsTable";
+import OperationsWidget from "./transaction-list/OperationsWidget";
+import ExchangeHistory from "./transaction-list/ExchangeHistory";
 import { BalanceCard } from "./balance/Card";
 import { ExternalRedirect } from "./ExternalRedirect";
 import { Loader } from "./Loader";
 
-export { PageTitle, TransactionsTable, Container, BalanceCard, ExternalRedirect, Loader };
+export {
+	PageTitle,
+	OperationsWidget,
+	ExchangeHistory,
+	Container,
+	BalanceCard,
+	ExternalRedirect,
+	Loader,
+};
