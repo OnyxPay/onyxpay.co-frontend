5253f1a058ba2b2e7931d9e8e49affe66f362f56
Anna Korunska <akorunska@gmail.com>
2019-08-19 15:03:08 +0300

    referral program page created

---
+++ src/pages/referral-program/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { PageTitle } from "components";
+
+class ReferralProgram extends Component {
+	render() {
+		return (
+			<>
+				<PageTitle>Referral Program Info</PageTitle>
+			</>
+		);
+	}
+}
+
+export default ReferralProgram;
