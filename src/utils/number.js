0ff341d6decda1527e82d4532398b53081266ce9
OnyxPayDev <50660398+OnyxPayDev@users.noreply.github.com>
2019-08-20 10:27:18 +0300

    ONXP-1088 [Currency] Reduce number of decimals in currencies to 8 (#167)
    * ONXP-1088 [Currency] Reduce number of decimals in currencies to 8
    
    * return fixed number of decimals string

--- src/utils/number.js
+++ src/utils/number.js
@@ -21,11 +21,11 @@ export function convertAsset(asset, rate) {
 	let excRate = new BigNumber(rate.rate);
 	excRate = excRate.div(Math.pow(10, rate.decimals));
 
-	return amountBN.times(excRate).toString();
+	return amountBN.times(excRate).toFixed(8);
 }
 
 export function addAmounts(a, b) {
-	return new BigNumber(a).plus(new BigNumber(b)).toString();
+	return new BigNumber(a).plus(new BigNumber(b)).toFixed(8);
 }
 
 export function minus(a, b) {
