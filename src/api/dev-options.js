59357f88802c956c7b86500018d1ef1c4a8d3bab
Vladimir Stavitskiy <vladimir.stavitskiy@apriorit.com>
2019-08-29 16:59:50 +0300

    implement setting of mode for RequestHolder contract

---
+++ src/api/dev-options.js
@@ -0,0 +1,59 @@
+import { ParameterType } from "ontology-ts-sdk";
+
+import { unlockWalletAccount } from "./wallet";
+import { getStore } from "../store";
+import { resolveContractAddress } from "../redux/contracts";
+import { ContractAddressError } from "../utils/custom-error";
+import { createTrx, sendTrx, addSignAndSendTrx } from "./bc";
+
+import { get } from "lodash";
+
+export async function changeMode(newMode) {
+	const { pk, accountAddress } = await unlockWalletAccount();
+	const store = getStore();
+	const address = await store.dispatch(resolveContractAddress("RequestHolder"));
+	if (!address) {
+		throw new ContractAddressError("Unable to get address of RequestHolder smart-contract");
+	}
+
+	const trx = createTrx({
+		funcName: "ChangeMode",
+		params: [
+			{ label: "caller", type: ParameterType.String, value: "did:onx:" + accountAddress.value },
+			{ label: "keyNo", type: ParameterType.Integer, value: 1 },
+			{
+				label: "mode",
+				type: ParameterType.Int,
+				value: newMode,
+			},
+		],
+		contractAddress: address,
+		accountAddress,
+	});
+
+	return addSignAndSendTrx(trx.serialize(), pk);
+}
+
+export async function getMode(userId) {
+	const store = getStore();
+	const address = await store.dispatch(resolveContractAddress("RequestHolder"));
+	if (!address) {
+		throw new ContractAddressError("Unable to get address of RequestHolder smart-contract");
+	}
+
+	const trx = createTrx({
+		funcName: "GetMode",
+		params: [],
+		contractAddress: address,
+	});
+
+	const res = await sendTrx(trx, true);
+
+	let mode = get(res, "Result.Result", 0);
+	if (mode === "") {
+		mode = 0;
+	} else {
+		mode = parseInt(mode, 16);
+	}
+	return mode;
+}
